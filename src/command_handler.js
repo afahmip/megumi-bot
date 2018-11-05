f855df0558add10098837adea22988c43fdd9ce5
ahmadfahmip <afahmi3@yahoo.co.id>
2018-11-05 20:35:13 +0700

    Add data penyakit

--- src/command_handler.js
+++ src/command_handler.js
@@ -3,13 +3,17 @@
 const data_health = require('./data_health.json'),
       data_aktivitas = require('./data_aktivitas.json'),
       data_daftardok = require('./data_daftardok.json'),
-      data_dokter = require('./data_dokter.json');
+      data_dokter = require('./data_dokter.json'),
+      data_appoint = require('./data_appoint.json'),
+      data_penyakit = require('./data_penyakit.json');
 
 const command_list = {
     'Tampilin data kesehatanku dong': data_health,
     'Minta rekomendasi aktivitas dong': data_aktivitas,
     'Mau bikin appointment sama dokter nih': data_daftardok,
-    'Pilih dr. Winnie Fiona, Sp.A': data_dokter
+    'Pilih dr. Winnie Fiona, Sp.A': data_dokter,
+    'Buat appointment': data_appoint,
+    'Tampilkan gejala demam': data_penyakit
 }
 
 let handle_command = (input) => {
