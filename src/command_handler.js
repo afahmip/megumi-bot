f6790f2771db7a5348240f2c5d11a54f87105417
ahmadfahmip <afahmi3@yahoo.co.id>
2018-11-06 19:43:05 +0700

    Add daftar dokter

--- src/command_handler.js
+++ src/command_handler.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const data_health = require('./data_health.json'),
-      data_aktivitas = require('./data_aktivitas.json');
+      data_aktivitas = require('./data_aktivitas.json'),
+      data_daftardok = require('./data_daftardok.json'),
+      data_dokter = require('./data_dokter.json');
 
 const command_list = {
     'Tampilin data kesehatanku dong': data_health,
-    'Minta rekomendasi aktivitas dong': data_aktivitas
+    'Minta rekomendasi aktivitas dong': data_aktivitas,
+    'Mau bikin appointment sama dokter nih': data_daftardok,
+    'Pilih dr. Winnie Fiona, Sp.A': data_dokter
 }
 
 let handle_command = (input) => {
