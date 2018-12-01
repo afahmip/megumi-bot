77605aa565ff9e4c65581f87d751b136a5565529
JessinRa <jessin.bdg@gmail.com>
2018-12-01 14:10:24 +0700

    case insensitive, edit alttext from flex message

--- src/command_handler.js
+++ src/command_handler.js
@@ -38,48 +38,48 @@ const input_riwayatok = {
 };
 
 const command_list = {
-    'Hai Megumi!': data_hello,
-    'Kondisiku sekarang gimana ya?': data_kesehatan_bagus,
-    'Tampilin data kesehatanku dong': data_health,
-    'Minta rekomendasi aktivitas dong': data_aktivitas,
-    'Tampilkan rekomendasi aktivitas': data_aktivitas,
+    'hai megumi!': data_hello,
+    'kondisiku sekarang gimana ya?': data_kesehatan_bagus,
+    'tampilin data kesehatanku dong': data_health,
+    'minta rekomendasi aktivitas dong': data_aktivitas,
+    'tampilkan rekomendasi aktivitas': data_aktivitas,
 
     // =============  Cluster make appointment  =======================
 
-    'Mau bikin appointment sama dokter nih': data_daftardok,
-    'Buat appointment': data_daftardok,
+    'mau bikin appointment sama dokter nih': data_daftardok,
+    'buat appointment': data_daftardok,
 
-    'Tampilkan dr. Winnie Fiona, Sp.A': data_dokter_01,
-    'Tampilkan dr. Jonathan Pierro': data_dokter_02,
+    'tampilkan dr. winnie fiona, sp.a': data_dokter_01,
+    'tampilkan dr. jonathan pierro': data_dokter_02,
 
-    'Schedule dr. Winnie Fiona, Sp.A': data_show_schedule,
-    'Schedule dr. Jonathan Pierro': data_show_schedule,
+    'schedule dr. winnie Fiona, sp.a': data_show_schedule,
+    'schedule dr. jonathan pierro': data_show_schedule,
 
-    'Appointment 3 December 2018 12:00': data_result_appointment,
-    'Appointment 3 December 2018 15:00': data_request_reschedule,
-    'Reschedule ke jam 15:00 dong': data_request_reschedule,
+    'appointment 3 desember 2018 12:00': data_result_appointment,
+    'appointment 3 desember 2018 15:00': data_request_reschedule,
+    'reschedule ke jam 15:00 dong': data_request_reschedule,
 
-    'Reschedule 3 December 2018 15:00': data_result_reschedule,
-    'Cek appointment': data_show_appointment,
-    'Tampilkan appointment': data_show_appointment,
+    'reschedule 3 desember 2018 15:00': data_result_reschedule,
+    'cek appointment': data_show_appointment,
+    'tampilkan appointment': data_show_appointment,
 
-    'Cancel appointment': data_request_cancel,
-    'Cancel 3 December 2018 15:00': data_result_cancel_appointment,
+    'cancel appointment': data_request_cancel,
+    'cancel 3 desember 2018 15:00': data_result_cancel_appointment,
 
     // ================================================================
-    'Tampilkan gejala demam': data_penyakit,
-    'Kebiasaan hidupku selama ini gimana ya, megumi?': data_riwayat,
-    'Makasih ya megumi :)': data_thank,
-    'Megumi, aku habis sakit nih': input_riwayat,
-    'Tambah riwayat penyakit': input_riwayat,
-    'Tampilin riwayat kesehatanku dong': data_daftarpenyakit,
-    'Tampilkan riwayat kesehatan': data_daftarpenyakit,
-    'Aku kemarin habis patah tulang lagi nih': input_riwayatdetil,
-    'Abis kecelakaan, nabrak mobil gitu, kena kepala': input_riwayatok
+    'tampilkan gejala demam': data_penyakit,
+    'kebiasaan hidupku selama ini gimana ya, megumi?': data_riwayat,
+    'makasih ya megumi :)': data_thank,
+    'megumi, aku habis sakit nih': input_riwayat,
+    'tambah riwayat penyakit': input_riwayat,
+    'tampilin riwayat kesehatanku dong': data_daftarpenyakit,
+    'tampilkan riwayat kesehatan': data_daftarpenyakit,
+    'aku kemarin habis patah tulang lagi nih': input_riwayatdetil,
+    'abis kecelakaan, nabrak mobil gitu, kena kepala': input_riwayatok
 };
 
 let handle_command = (input) => {
-    return command_list[input];
+    return command_list[input.toLowerCase()];
 };
 
 module.exports = {
